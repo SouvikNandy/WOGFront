91045a54de073d2d6feede252464a0eeae0b4718
Souvik Nandy <souvikpxnandy@gmail.com>
2020-07-19 12:45:21 +0530

    user view mwnu list done

--- src/pages/NewsFeeds.js
+++ src/pages/NewsFeeds.js
@@ -3,19 +3,22 @@ import '../assets/css/newsfeeds.css';
 import w1 from "../assets/images/wedding1.jpg";
 import {TiEdit} from "react-icons/ti";
 import { FaCameraRetro } from 'react-icons/fa';
+import {BsClockHistory, BsBookmarks} from 'react-icons/bs';
+import {AiOutlineUsergroupAdd, AiOutlineStar, AiOutlinePoweroff} from 'react-icons/ai';
+import {FiUnlock, FiSettings} from 'react-icons/fi';
 
 export class NewsFeeds extends Component {
     render() {
         return (
             <div className="nf-container">
                 <div className="nf-user-menu">
+                    <div className="nf-user-menu-overlay"></div>
                     <div className="nf-user-edit">
                         <div className="nf-user-img">
                             <img className="cube-user-img " src={w1} alt=""/>
                             <span className="edit-pic">
                                 <FaCameraRetro  className="cam-icon"/>
                             </span>
-                            
                         </div>
                         
                         <span className="m-display-name">
@@ -27,6 +30,42 @@ export class NewsFeeds extends Component {
 
                     </div>
                     <div className="nf-user-menulist">
+                        <div className="nf-upper-tokens">
+                            <div className="nf-menu-tokens">
+                                <BsClockHistory className="ico" />
+                                <span>Your Activities</span>
+                            </div>
+                            <div className="nf-menu-tokens">
+                                <AiOutlineUsergroupAdd className="ico" />
+                                <span>Discover People</span>
+                            </div>
+                            <div className="nf-menu-tokens">
+                                <AiOutlineStar className="ico" />
+                                <span>Ratings & Reviews</span>
+                            </div>
+                            <div className="nf-menu-tokens">
+                                <BsBookmarks className="ico" />
+                                <span>Saved</span>
+                            </div>
+
+                        </div>
+                        <div className="nf-lower-tokens">
+                            <div className="nf-menu-tokens">
+                                <FiUnlock className="ico" />
+                                <span>Privacy</span>
+                            </div>
+                            <div className="nf-menu-tokens">
+                                <FiSettings className="ico" />
+                                <span>Settings</span>
+                            </div>
+                            <div className="nf-menu-tokens">
+                                <AiOutlinePoweroff className="ico" />
+                                <span>Log Out</span>
+                            </div>
+
+                        </div>
+                        
+
 
                     </div>
                 </div>
