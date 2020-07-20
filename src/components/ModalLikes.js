09bfdcaa7d1973b62e2f5805bcb8757791db372b
Souvik Nandy <souvikpxnandy@gmail.com>
2020-07-20 08:08:46 +0530

    save post btn

--- src/components/ModalLikes.js
+++ src/components/ModalLikes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../assets/css/shotmodalview.css';
 
-import { FaHeart, FaPaperPlane, FaRegHeart, FaRegComment, FaRegPaperPlane } from "react-icons/fa";
+import { FaHeart, FaPaperPlane, FaRegHeart, FaRegComment, FaRegPaperPlane, FaBookmark, FaRegBookmark } from "react-icons/fa";
 
 const ModalLikes = (props) => {
     let likeBtn = props.isLiked ?
@@ -13,6 +13,7 @@ const ModalLikes = (props) => {
                 <button className="btn-anc">{likeBtn}</button>
                 <button className="btn-anc" onClick={feedCommentBox}><FaRegComment className="icons" /></button>
                 <button className="btn-anc"><FaRegPaperPlane className="icons" /></button>
+                <button className="btn-anc"><FaRegBookmark className="icons" /></button>
             </div>
             <div className="m-likes-preview">
                 <FaHeart className="icons" /><span>{props.responsecounts.likes}</span>
